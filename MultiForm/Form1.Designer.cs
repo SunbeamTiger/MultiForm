e9b77333d1e5cb30d745440a93534e8399ed4c9e
DanHagen <dan.hagen@centralia.edu>
2024-01-30 09:28:24 -0800

    Add project files.

---
+++ MultiForm/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace MultiForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShowForm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnShowForm
+            // 
+            this.btnShowForm.Location = new System.Drawing.Point(199, 124);
+            this.btnShowForm.Name = "btnShowForm";
+            this.btnShowForm.Size = new System.Drawing.Size(185, 42);
+            this.btnShowForm.TabIndex = 0;
+            this.btnShowForm.Text = "Show form";
+            this.btnShowForm.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 315);
+            this.Controls.Add(this.btnShowForm);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShowForm;
+    }
+}
+
